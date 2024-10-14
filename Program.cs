eee38cc8f19463e0fce4b82aef677fd4bf01b204
Mojid Eroje <majidy287@gmail.com>
2024-10-14 05:10:52 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DNA
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // DNA strand
+      string startStrand = "ATGCGATGAGCTTAC";
+
+      // Find location of "tga"
+      int tga = startStrand.IndexOf("TGA");
+
+      // Start point and stop point variables
+      int startPoint = 0;
+      int length = tga + 3;
+
+      // Define final strand
+      string dna = startStrand.Substring(startPoint, length);
+      Console.WriteLine(dna);
+
+      // DNA mutation search
+      Console.WriteLine(dna[3]);
+
+    }
+  }
+}
